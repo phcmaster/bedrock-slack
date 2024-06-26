8162de6d1546b11b64e562a54edf7fd0093ed4ef
Pedro Henrique Oliveira <39164505+phcmaster@users.noreply.github.com>
2024-06-26 12:06:19 -0300

    Update lambda function with logs

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ import os
 import boto3
 import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 MAX_TOKENS=150
 SLACK_POST_URL = os.environ["WEB_HOOK_URL_DEV"]
@@ -19,24 +21,19 @@ bedrock = boto3.client('bedrock-runtime')
 
 def lambda_handler(event, context):
     
-    print("evento: {}".format(event))
+    logger.info('event: %s', event)
     
     user_prompt = event['event']['text']
     channel = event['event']['channel']
     user = event['event']['user']
     
-    print("Body: {}".format(user_prompt))
-    print("Channel: {}".format(channel))
+    logger.info('Body: %s', user_prompt)
+    logger.info('Channel: %s', channel)
     
     message = calling_bedrock(user_prompt)
-    
-    #message = 'Hello teste!'
-    
-    #response = format_post_message(user, channel, message)
     response = format_webhook(user, message)
     
     post_message_to_slack(response)
-
 
 
 def exception_handler(e, status_code, message):
@@ -70,46 +67,44 @@ def format_post_message(user, channel, message):
 
 
 
-def calling_bedrock(prompt_data):
-    
-    print('Calling Amazon Bedrock API')
+def calling_bedrock(prompt_data):  
     
     #formatting body for claude
     body = json.dumps({"prompt": "Human:"+prompt_data+"\nAssistant:", "max_tokens_to_sample":MAX_TOKENS})
-    
-    print('Bedrock body: {}'.format(body))
+    logger.info('Amazon Bedrock prompt: %s', body)
     
     #invoke model
     try:
+        logger.info('Calling Amazon Bedrock API')
         response = bedrock.invoke_model(body=body, modelId=modelId, accept=accept, contentType=contentType)
     except Exception as e:
-        return exception_handler(e, 500, 'Falha ao executar chamada para o Bedrock!')
+        logger.error('Failed to execute call to Amazon Bedrock! Error: %s', str(e))
+        return exception_handler(e, 500, 'Failed to execute call to Amazon Bedrock!')
     
     if 'body' in response and response['body'] is not None:
         response_body = json.loads(response['body'].read())
         message = response_body.get('completion', 'Error: Unexpected Bedrock response format.')
-        print('Bedrock response: {}'.format(message))
+        logger.info('Bedrock response: %s', message)
         return message
     else:
+        logger.error('Invalid Bedrock response')
         return exception_handler(ValueError("Invalid Bedrock response"), 500, 'Failed to execute Bedrock call!')
 
 
 def post_message_to_slack(response):
     
-    print("message for post message: {}".format(response))
+    logger.info('Message for post message: %s', response)
     
     try:
-        #hdr = {'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + SLACK_BOT_TOKEN}
         hdr = {'Content-Type': 'application/json; charset=utf-8'}
         req = urllib.request.Request(SLACK_POST_URL, json.dumps(response).encode('utf-8'), hdr)
         response_webhook = urllib.request.urlopen(req).read()
-        logger.info("message for post message: {}".format(response_webhook))
+        logger.info('message for post message: %s', response_webhook)
     except Exception as e:
-        return exception_handler(e, 500, 'Falha ao executar a função! Tente novamente!')
+        logger.error('Failed to execute the function! Error: %s', str(e))
+        return exception_handler(e, 500, 'Failed to execute the function! Please, try again!')
 
 
- 
- 
  
     
 #SLACK - Event subscription challenge validator
@@ -124,7 +119,7 @@ def post_message_to_slack(response):
 #     }
 
 
-
+# Working with stream
  # stream = response.get('body')
         
         # if stream:
@@ -138,3 +133,6 @@ def post_message_to_slack(response):
         #         print('Bedrock stream response: {}'.format(response))
         
         # return response
+
+#message = 'Hello teste!'
+#response = format_post_message(user, channel, message)
